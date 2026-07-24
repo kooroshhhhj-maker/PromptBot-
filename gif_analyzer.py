3ee31f8a1be46079089dd04e524b70fe85fcbf30
Koorosh <Kooroshhhhj@gmail.com>
2026-07-24 03:34:24 +0330

    Support Telegram MP4 animations

--- gif_analyzer.py
+++ gif_analyzer.py
@@ -1,16 +1,29 @@
 import io
+import cv2
 from PIL import Image
 
 
-def extract_gif_frame(gif_bytes):
-    gif = Image.open(io.BytesIO(gif_bytes))
+def extract_gif_frame(video_bytes):
+    video_path = "/tmp/gif.mp4"
 
-    gif.seek(0)
+    with open(video_path, "wb") as f:
+        f.write(video_bytes)
 
-    frame = gif.convert("RGB")
+    cap = cv2.VideoCapture(video_path)
+
+    success, frame = cap.read()
+
+    cap.release()
+
+    if not success:
+        raise Exception("Could not read animation")
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    image = Image.fromarray(frame)
 
     output = io.BytesIO()
-    frame.save(output, format="JPEG")
+    image.save(output, format="JPEG")
     output.seek(0)
 
     return output
